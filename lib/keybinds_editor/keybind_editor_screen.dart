3f9fca6bf1cbd5fb46b90430e56fcefd8b237927
Matthew Kosarek <matt.kosarek@canonical.com>
2025-04-13 16:44:50 -0400

    Revert

--- lib/keybinds_editor/keybind_editor_screen.dart
+++ lib/keybinds_editor/keybind_editor_screen.dart
@@ -6,13 +6,11 @@ class KeybindEditorScreen extends StatefulWidget {
   const KeybindEditorScreen({
     required this.config,
     this.index,
-    this.isBuiltIn = false,
     super.key,
   });
 
   final MiracleConfigData config;
   final int? index;
-  final bool isBuiltIn;
 
   @override
   State<KeybindEditorScreen> createState() => _KeybindEditorScreenState();
@@ -27,28 +25,16 @@ class _KeybindEditorScreenState extends State<KeybindEditorScreen> {
   @override
   void initState() {
     super.initState();
-    if (widget.isBuiltIn) {
-      final existingCommand = widget.index != null
-          ? widget.config.getBuiltInKeyCommands()[widget.index!]
-          : null;
-      _selectedAction = existingCommand?.action ??
-          MiracleConfig.getKeyboardActionsOptions().first.value;
-      _selectedModifiers = existingCommand?.modifiers ?? 0;
-      _selectedKey = existingCommand?.key ?? 0;
-      _selectedBuiltInAction = existingCommand?.builtInAction ??
-          MiracleConfig.getBuiltInKeyCommandsOptions().first.value;
-    } else {
-      final existingCommand = widget.index != null
-          ? widget.config.getCustomKeyCommands()[widget.index!]
-          : null;
-      _selectedAction = existingCommand?.action ??
-          MiracleConfig.getKeyboardActionsOptions().first.value;
-      _selectedModifiers = existingCommand?.modifiers ?? 0;
-      _selectedKey = existingCommand?.key ?? 0;
-      _commandController = TextEditingController(
-        text: existingCommand?.command ?? '',
-      );
-    }
+    final existingCommand = widget.index != null
+        ? widget.config.getCustomKeyCommands()[widget.index!]
+        : null;
+    _selectedAction = existingCommand?.action ??
+        MiracleConfig.getKeyboardActionsOptions().first.value;
+    _selectedModifiers = existingCommand?.modifiers ?? 0;
+    _selectedKey = existingCommand?.key ?? 0;
+    _commandController = TextEditingController(
+      text: existingCommand?.command ?? '',
+    );
   }
 
   @override
@@ -58,47 +44,29 @@ class _KeybindEditorScreenState extends State<KeybindEditorScreen> {
   }
 
   void _saveKeybind() {
-    if (widget.isBuiltIn) {
-      if (widget.index != null) {
-        widget.config.updateBuiltInKeyCommand(
-          widget.index!,
-          _selectedAction,
-          _selectedModifiers,
-          _selectedKey,
-          _selectedBuiltInAction,
-        );
-      } else {
-        widget.config.addBuiltInKeyCommand(
-          _selectedAction,
-          _selectedModifiers,
-          _selectedKey,
-          _selectedBuiltInAction,
-        );
-      }
-    } else {
-      if (_commandController.text.isEmpty) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Please enter a command')),
-        );
-        return;
-      }
+    if (_commandController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Please enter a command')),
+      );
+      return;
+    }
 
-      if (widget.index != null) {
-        widget.config.editCustomKeyCommand(
-          widget.index!,
-          _selectedAction,
-          _selectedModifiers,
-          _selectedKey,
-          _commandController.text,
-        );
-      } else {
-        widget.config.addCustomKeyCommand(
-          _selectedAction,
-          _selectedModifiers,
-          _selectedKey,
-          _commandController.text,
-        );
-      }
+    if (widget.index != null) {
+      // Edit existing command
+      widget.config.editCustomKeyCommand(
+        widget.index!,
+        _selectedAction,
+        _selectedModifiers,
+        _selectedKey,
+        _commandController.text,
+      );
+    } else {
+      widget.config.addCustomKeyCommand(
+        _selectedAction,
+        _selectedModifiers,
+        _selectedKey,
+        _commandController.text,
+      );
     }
 
     Navigator.of(context).pop();
@@ -136,36 +104,14 @@ class _KeybindEditorScreenState extends State<KeybindEditorScreen> {
               },
             ),
             const SizedBox(height: 20),
-            if (widget.isBuiltIn)
-              DropdownButtonFormField<int>(
-                value: _selectedBuiltInAction,
-                items: MiracleConfig.getBuiltInKeyCommandsOptions().map((option) {
-                  return DropdownMenuItem<int>(
-                    value: option.value,
-                    child: Text(option.name),
-                  );
-                }).toList(),
-                onChanged: (value) {
-                  if (value != null) {
-                    setState(() {
-                      _selectedBuiltInAction = value;
-                    });
-                  }
-                },
-                decoration: const InputDecoration(
-                  labelText: 'Action',
-                  border: OutlineInputBorder(),
-                ),
+            TextField(
+              controller: _commandController,
+              decoration: const InputDecoration(
+                labelText: 'Command',
+                hintText: 'Enter the command to execute',
+                border: OutlineInputBorder(),
               ),
-            if (!widget.isBuiltIn)
-              TextField(
-                controller: _commandController,
-                decoration: const InputDecoration(
-                  labelText: 'Command',
-                  hintText: 'Enter the command to execute',
-                  border: OutlineInputBorder(),
-                ),
-              ),
+            ),
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _saveKeybind,
